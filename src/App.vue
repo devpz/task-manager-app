d9753746edc750b45c39d0558d54da632b3abd3f
admin <mac@mb14.local>
2024-01-31 17:55:14 +0100

    Added integration with Firebase

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,57 @@
 <template>
-  <div class="container mt-5">
-    <div class="row">
-      <div class="col-md-6">
-        <h2>Task Manager</h2>
-        <TaskForm @add="addTask" />
-        <TaskList :tasks="tasks" @remove="removeTask" />
-      </div>
-    </div>
+  <div id="app">
+    <h2>{{ title }}</h2>
+    <input id="newtasks" v-model="newtasks" />&nbsp;
+    <button @click="addNewtasks()">Add tasks</button>
+    <p v-for="note in tasks" :key="note.id">
+      <b>* {{ note.description }}</b>&nbsp;
+      <button @click="deletetasks(note.id)">Delete tasks</button>
+    </p>
   </div>
 </template>
 
 <script>
-import TaskForm from "./components/TaskForm.vue";
-import TaskList from "./components/TaskList.vue";
+import { ref, onMounted } from 'vue';
+import { getFirestore, collection, addDoc, deleteDoc, doc, getDocs } from 'firebase/firestore';
+import { initializeApp } from 'firebase/app';
+
+import { firebaseConfig } from './firebaseConfig'; 
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 export default {
-  data() {
-    return {
-      tasks: [],
+  setup() {
+    const title = ref('Task manager app');
+    const tasks = ref([]);
+    const newtasks = ref('');
+
+    const refreshData = async () => {
+      tasks.value = [];
+      const querySnapshot = await getDocs(collection(db, 'tasks'));
+      querySnapshot.forEach((doc) => {
+        tasks.value.push({ description: doc.data().description, id: doc.id });
+      });
     };
-  },
-  methods: {
-    addTask(newTask) {
-      this.tasks.push(newTask);
-    },
-    removeTask(index) {
-      this.tasks.splice(index, 1);
-    },
-  },
-  components: {
-    TaskForm,
-    TaskList,
+
+    const addNewtasks = async () => {
+      await addDoc(collection(db, 'tasks'), { description: newtasks.value });
+      newtasks.value = ''; // Clear the input field
+      refreshData();
+    };
+
+    const deletetasks = async (id) => {
+      await deleteDoc(doc(db, 'tasks', id));
+      refreshData();
+    };
+
+    onMounted(() => {
+      refreshData();
+    });
+
+    return { title, tasks, newtasks, refreshData, addNewtasks, deletetasks };
   },
 };
 </script>
+
+
---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyB5VvDczwW51KRbYqmZJIfpoxfQ6P2USl0",
+  authDomain: "task-manager-app-5b69c.firebaseapp.com",
+  databaseURL: "https://task-manager-app-5b69c-default-rtdb.firebaseio.com",
+  projectId: "task-manager-app-5b69c",
+  storageBucket: "task-manager-app-5b69c.appspot.com",
+  messagingSenderId: "726844969703",
+  appId: "1:726844969703:web:56379bde2d01350c100842"
+};
+

f47649a221af11799767338b818b609b2b9bf41b
admin <mac@mb14.local>
2024-02-03 18:39:35 +0100

    Added styling with Vuetify Component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,86 @@
 <template>
-  <div id="app">
-    <h2>{{ title }}</h2>
-    <input id="newtasks" v-model="newtasks" />&nbsp;
-    <button @click="addNewtasks()">Add tasks</button>
-    <p v-for="note in tasks" :key="note.id">
-      <b>* {{ note.description }}</b>&nbsp;
-      <button @click="deletetasks(note.id)">Delete tasks</button>
-    </p>
-  </div>
+  <v-app>
+    <v-main>
+      
+  <v-card max-width="996" class="mx-auto" color="grey-lighten-3">
+    <v-layout>
+      <v-app-bar
+        color="teal-darken-4"
+        image="https://picsum.photos/1920/1080?random"
+      >
+        <template v-slot:image>
+          <v-img
+            gradient="to top right, rgba(19,84,122,.8), rgba(128,208,199,.8)"
+          ></v-img>
+        </template>
+
+        <template v-slot:prepend>
+          <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        </template>
+
+        <v-app-bar-title>Task manager app</v-app-bar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>mdi-heart</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+      </v-app-bar>
+      
+
+      <v-main>
+        <v-container fluid>
+          
+          <v-row dense>
+
+            
+  <v-text-field id="newtasks" label="New task" v-model="newtasks"> 
+    <template v-slot:append>
+      <v-btn @click="addNewtasks()">
+        Add
+      </v-btn>
+    </template>
+   
+  </v-text-field>
+
+            <v-col
+              v-for="note in tasks"
+              :key="note.id"
+              cols="12"
+            >
+            
+              <v-card class="d-flex" v-slot:append
+                :title="`${note.description}`"><v-btn  v-slot:append density="compact"  icon="mdi-check" @click="deletetasks(note.id)"></v-btn>
+          
+    
+              </v-card>
+              
+              
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-main>
+    </v-layout>
+  </v-card>
+    </v-main>  
+  </v-app>
+  
 </template>
 
 <script>
+
 import { ref, onMounted } from 'vue';
 import { getFirestore, collection, addDoc, deleteDoc, doc, getDocs } from 'firebase/firestore';
 import { initializeApp } from 'firebase/app';
+import HelloWorld from './components/HelloWorld.vue'
 
 import { firebaseConfig } from './firebaseConfig'; 
 
@@ -22,6 +89,7 @@ const db = getFirestore(app);
 
 export default {
   setup() {
+    
     const title = ref('Task manager app');
     const tasks = ref([]);
     const newtasks = ref('');
@@ -52,6 +120,9 @@ export default {
     return { title, tasks, newtasks, refreshData, addNewtasks, deletetasks };
   },
 };
+
+
+
+  
+
 </script>
-
-
